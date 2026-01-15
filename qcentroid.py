82051c6c61d9c7995d0741f7913571abf9dbc133
LKSNextQuantumGroup <aitormoreno@lksnext.com>
2026-01-15 12:35:47 +0100

    Update qcentroid.py

--- qcentroid.py
+++ qcentroid.py
@@ -1,4 +1,4 @@
-from qiskit import QuantumCircuit
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, transpile
 from qiskit_aer import Aer
 
 def run(input_data, solver_params, extra_arguments):
@@ -7,21 +7,17 @@ def run(input_data, solver_params, extra_arguments):
     # This is your solver's code
 
     #size = int(input_data['size'])
-    size=1
+    size=100
     backend = Aer.get_backend('qasm_simulator')
 
     qc = QuantumCircuit(1)
     qc.h(0)
     qc.measure_all()
-
-    job = execute(qc, backend=backend, shots=size, memory=True)
-    individual_shots = job.result().get_memory()
-
-    #logger.info("Ending Solver...")
-    shots = ''
-    for i in individual_shots:
-        shots += i
-
-    output = {'shots': shots}
+    
+    tqc = transpile(qc, backend)
+    result = backend.run(tqc, shots=size).result()
+    counts = result.get_counts()
+    print(counts)
+    output = {'shots': counts}
     # And this is the output it returns, which is a dictionary
     return output
