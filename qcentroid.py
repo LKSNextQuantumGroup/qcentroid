3ce16f4414d7c1cd33d42e10988dc22560473283
LKSNextQuantumGroup <aitormoreno@lksnext.com>
2026-01-15 11:46:39 +0100

    Update qcentroid.py

--- qcentroid.py
+++ qcentroid.py
@@ -1,9 +1,10 @@
 def run(input_data, solver_params, extra_arguments):
-    logger.info("Starting Solver...")
+    #logger.info("Starting Solver...")
 
     # This is your solver's code
 
-    size = int(input_data['size'])
+    #size = int(input_data['size'])
+    size=1
     backend = Aer.get_backend('qasm_simulator')
 
     qc = QuantumCircuit(1)
@@ -13,7 +14,7 @@ def run(input_data, solver_params, extra_arguments):
     job = execute(qc, backend=backend, shots=size, memory=True)
     individual_shots = job.result().get_memory()
 
-    logger.info("Ending Solver...")
+    #logger.info("Ending Solver...")
     shots = ''
     for i in individual_shots:
         shots += i
